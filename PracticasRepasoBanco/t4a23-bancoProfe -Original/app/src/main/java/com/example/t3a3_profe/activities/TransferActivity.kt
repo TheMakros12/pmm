887638bae2cefa44ad7d67aa8271286e7bcdbbff
TheMakros12 <marcosnbx@gmail.com>
2026-02-18 20:37:51 +0100

    hola

--- PracticasRepasoBanco/t4a23-bancoProfe -Original/app/src/main/java/com/example/t3a3_profe/activities/TransferActivity.kt
+++ PracticasRepasoBanco/t4a23-bancoProfe -Original/app/src/main/java/com/example/t3a3_profe/activities/TransferActivity.kt
@@ -1,6 +1,8 @@
 package com.example.t3a3_profe.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +20,37 @@ class TransferActivity : AppCompatActivity() {
 
         binding = ActivityTransferBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setupSpinnerCuentaPropia()
+        configurarRadioButtons()
+    }
+
+    private fun setupSpinnerCuentaPropia() {
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.cuentas_array,
+            android.R.layout.simple_spinner_item
+        )
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.spinnerCuentaOrigen.adapter = adapter
+        binding.spinnerCuentaPropia.adapter = adapter
+    }
+
+    private fun configurarRadioButtons() {
+        binding.radioGroupDestino.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId) {
+                R.id.rdBtnCuentaPropia -> {
+                    binding.spinnerCuentaPropia.visibility = View.VISIBLE
+                    binding.tiLayoutCuentaAjena.visibility = View.GONE
+                }
+                R.id.rdBtnCuentaAjena -> {
+                    binding.spinnerCuentaPropia.visibility = View.GONE
+                    binding.tiLayoutCuentaAjena.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 
 }
